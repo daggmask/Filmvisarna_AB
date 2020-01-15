9d1c525d59f86619dbf93e670835215464d17bf7
snigelkott <dahlstedt.per@gmail.com>
2020-01-15 15:52:20 +0100

    Movielist skeleton
    Mob-coding

---
+++ filmvisarna_ab/src/views/movieList.vue
@@ -0,0 +1,49 @@
+<template>
+  <ul>
+      <li v-for="movie in movies"
+        v-bind:key="movie.title"
+        class="container">
+        <div class="container">
+            img
+        </div>
+        <div class="container">
+            <h2>{{ movie.title }}</h2>
+            <p>{{ movie.genre }}</p>
+        </div>
+      </li>
+  </ul>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            movies:[
+                {
+                    title: 'grinch',
+                    genre: 'comedy',
+                },
+                {
+                    title: 'Dagboken',
+                    genre: 'drama'
+                },
+                {
+                    title: 'afterlife',
+                    genre: 'action'
+                },
+
+            ]
+        }
+    }
+
+}
+</script>
+
+<style>
+*{
+    box-sizing: border-box;
+}
+.container{
+    display: flex;
+}
+</style>

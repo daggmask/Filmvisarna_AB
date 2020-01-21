62545a3c08ad3259785203ce64ab73ec0ca6416a
Fredrik Jönsson <Fredrikjay@gmail.com>
2020-01-21 11:22:29 +0100

    created header component

--- filmvisarna_ab/src/App.vue
+++ filmvisarna_ab/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <a @click="publishMovies" class="btn-floating pulse"><i class="material-icons">publish</i></a>
-    <!-- Header -->
+    <Mheader></Mheader>
     <router-view></router-view>
     <!-- Footer -->
     <!-- Menu -->
@@ -9,11 +9,15 @@
 </template>
 
 <script>
+import Mheader from '@/components/header.vue'
 export default {
           methods:{
       publishMovies(){
         this.$store.dispatch("publishMovies")
       }
+    },
+    components: {
+      Mheader
     }
 }
 </script>
---
+++ filmvisarna_ab/src/components/header.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="head">
+<h1>Filmvisarna</h1>
+</div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Righteous&display=swap');
+
+*{
+  box-sizing: border-box;
+}
+
+.head{
+    text-align: center;
+    margin-top:0%;
+    border-bottom-style: solid;
+    border-bottom-color:  red;
+}
+h1{
+font-family: 'Righteous', cursive;
+margin-top:0%;
+margin-bottom: 1%;
+}
+
+</style>

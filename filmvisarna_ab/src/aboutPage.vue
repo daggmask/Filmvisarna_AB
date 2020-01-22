985cf177829c67ea914afbbdc02f77dd8c84af2c
Fredrik Jönsson <Fredrikjay@gmail.com>
2020-01-22 09:05:22 +0100

    added google maps

--- filmvisarna_ab/src/aboutPage.vue
+++ filmvisarna_ab/src/aboutPage.vue
@@ -3,8 +3,11 @@
     <div class="text">
    <h1>Om Filmvisarna</h1>
    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo accusamus consequatur minima voluptates a autem fuga consectetur similique deleniti fugiat provident rerum voluptate, dignissimos quis natus est ex tempora vitae earum quisquam excepturi possimus. Aliquam, dolorum recusandae reiciendis dolorem, incidunt dolores est adipisci eos aspernatur quisquam eius, atque alias architecto?</p>
-   <h4>Hitta Hit:</h4>
+   
 </div>
+<h4>Hitta Hit:</h4>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2248.8428505604247!2d13.209326015928184!3d55.69171988053711!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4653979677c463cf%3A0xdc335a111238b2d7!2sKalkstensv%C3%A4gen%203%2C%20224%2078%20Lund!5e0!3m2!1ssv!2sse!4v1579679349171!5m2!1ssv!2sse" width="450" height="450" frameborder="0" style="border:0;" allowfullscreen="">
+   </iframe>
 </div>
 </template>
 

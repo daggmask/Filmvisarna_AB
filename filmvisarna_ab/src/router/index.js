934fb42a8ac25095075c49a1e4257138b597a861
Fredrik Jönsson <Fredrikjay@gmail.com>
2020-01-22 10:46:30 +0100

    change routing to orginal

--- filmvisarna_ab/src/router/index.js
+++ filmvisarna_ab/src/router/index.js
@@ -3,12 +3,18 @@ import VueRouter from 'vue-router'
 import MovieList from '../views/movieList.vue'
 import Movie from '../views/movieShowing.vue'
 import aboutPage from '@/views/aboutPage.vue'
+import homePage from '@/views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    name: 'homePage',
+    component: homePage
+  },
+  {
+    path: '/about',
     name: 'aboutPage',
     component: aboutPage
   },

d5803335a0d2e6bb2d1767d4e6cc841074f1b9b0
snigelkott <dahlstedt.per@gmail.com>
2020-01-22 10:48:49 +0100

    added html

--- filmvisarna_ab/src/components/screenings.vue
+++ filmvisarna_ab/src/components/screenings.vue
@@ -1,5 +1,22 @@
 <template>
-  
+  <div id="screenings-component">
+    <ul>
+      <li
+      v-for="(date, i) in dates"
+      :key="i + date">
+        <div>{{ date }}</div>
+        <ul>
+          <li
+          v-for="(screening, i) in screenings(date)"
+          :key="i + screening">
+            <span>{{ screening.time }}</span>
+            <span>{{ screening.auditorium }}</span>
+            <span>Platser kvar</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>

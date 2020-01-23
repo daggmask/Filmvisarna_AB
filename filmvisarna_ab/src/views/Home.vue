4d8e363ce00282c8382b57297972dac806f3760b
Fredrik Jönsson <Fredrikjay@gmail.com>
2020-01-23 15:05:41 +0100

    added movie title to movie slide and css

--- filmvisarna_ab/src/views/Home.vue
+++ filmvisarna_ab/src/views/Home.vue
@@ -8,11 +8,15 @@
     @click="toMovieShowing(movie)"  
     class="responsive-img pic">
 
+    <div class="movieTitle">
     <p v-for="(movie, i) in movies"
-    v-bind:key="movie.title + i"
-    class="movieTitle">
+    v-bind:key="movie.title + i">
     {{movie.title}}
     </p>
+    </div>
+   
+   
+      </figure>
   </div>
 
 </template>
@@ -46,16 +50,17 @@ export default {
 </script>
 
 <style scoped>
-
+@import url('https://fonts.googleapis.com/css?family=Lato&display=swap');
 #slider{
     overflow: hidden;
 }
-#slider figure{
+#slider figure {
     position: relative;
     width: 600%;
     margin: 0;
     left: 0;
     animation: 20s slider infinite;
+    height: 60vh;
 }
 #slider figure img{
     height: 50vh;
@@ -69,6 +74,22 @@ export default {
 100%{
     left: -500%;
 }
+}
+.movieTitle{
+  display: flex;
+  justify-content: space-around;
+  position: absolute;
+  align-items: flex-end;
+  width: 100%;
+  height: 60vh;
+  padding:0%;
+  font-family: 'Lato', sans-serif;
+  font-size: 1.5em
+}
+p{
+  width: 40vw;
+  text-align: center;
+  height: 10vh;
 }
 </style>
 

3b7ff44c3f42512b51822d0d8e19b0824bb501a9
snigelkott <dahlstedt.per@gmail.com>
2020-01-23 12:49:03 +0100

    new dropdown

--- filmvisarna_ab/src/components/screenings.vue
+++ filmvisarna_ab/src/components/screenings.vue
@@ -1,35 +1,40 @@
 <template>
-  <div id="screenings-component">
-    <ul>
-      <li
+  <div class="container col">
+    <div class="row no-margin">
+      <div class="btn light-blue darken-4 col s4 offset-s4"
+      @click="isOpen = !isOpen, active = active">
+        <span>Välj datum</span>
+      </div>
+    </div>
+    <div v-if="isOpen">
+      <div class="row no-margin"
       v-for="(date, i) in dates"
-      :key="i + date">
-        <div>{{ date }}</div>
-        <ul>
-          <li
-          v-for="(screening, i) in screenings(date)"
-          :key="i + screening">
-            <span>{{ screening.time }}</span>
-            <span>{{ screening.auditorium }}</span>
-            <span>Platser kvar</span>
-          </li>
-        </ul>
-      </li>
-    </ul>
+      :key="i + date"
+      @click="setDate(date), isOpen = !isOpen">
+        <div
+        class="btn light-blue darken-3 col s4 offset-s4 date"
+        >
+        {{ date }}
+        </div>
+      </div>
+    </div>
+    <div class="container  ">
+      
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    movie: Object,
+    
   },
   computed: {
     dates(){
       let screenings = this.$store.state.screenings;
       let dates = [];
       for(let screening of screenings) {
-        if(this.movie.title === screening.film){
+        if(this.movieTitle === screening.film){
           dates.push(screening.date);
         }
       }
@@ -42,19 +47,38 @@ export default {
       let screenings = this.$store.state.screenings;
       let movieScreenings = []
       for(let screening of screenings) {
-        if(screening.film === this.movie.title && screening.date === date) {
+        if(screening.film === this.movieTitle && screening.date === date) {
           movieScreenings.push(screening);
         }
       }
       return movieScreenings;
     },
+    setDate(date){
+      this.date = date;
+    }
   },
   created(){
     this.$store.dispatch("getScreenings");
   },
+  data() {
+    return {
+      movieTitle: "Parasit",
+      date: String,
+      isOpen: false,
+      active: false,
+    }
+  }
 }
 </script>
-
-<style>
-
+  
+<style scoped>
+  .date{
+    text-align: center;
+  }
+  .no-margin{
+    margin-bottom: 0px;
+  }
+  .btn:hover{
+    background-color: #1e88e5 !important;
+  }
 </style>

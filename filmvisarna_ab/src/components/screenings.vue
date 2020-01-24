5cddcbc0e5d473e97f18e241ff1313d862f5e708
snigelkott <dahlstedt.per@gmail.com>
2020-01-24 11:11:00 +0100

    increased btn size and removed redundant code

--- filmvisarna_ab/src/components/screenings.vue
+++ filmvisarna_ab/src/components/screenings.vue
@@ -1,44 +1,44 @@
 <template>
   <div class="container col screenings">
-    <div class="row">
-      <div class="col s12">
-        <a href="#"
-        class="btn light-blue darken-4 col s6 offset-s3"
-        @click="isOpen = !isOpen"
-        >
-          Välj datum
-        </a>
-      </div>
+
+    <div class="row date-btn-row">
+      <a href="#"
+      class="btn-large light-blue darken-4 col s12"
+      @click="isOpen = !isOpen"
+      >
+        <h5>Välj datum</h5>
+      </a>
     </div>
 
-    <div class="row"
-    v-if="isOpen">
-      <div class="col s12"
+    <div class="dates-div" v-if="isOpen">
+      <div class="row dates-row"
       v-for="(date, i) in dates"
       :key="i + date">
         <a href="#"
-        class="btn light-blue darken-2 col s6 offset-s3"
+        class="btn-large light-blue darken-2 col s12 date"
         @click="setDate(date), isOpen = !isOpen">
         {{ date }}
         </a>
       </div>
     </div>
 
-    <div class="row">
-      <div class="col s12">
-        <div class="col s8 offset-s2"
+    <div class="row" v-if="!isOpen">
+      <ul :class="{ hidden: !isOpen }" class="collection">
+        <li class="collection-item col s12 light-blue darken-1"
         v-for="(screening, i) in screeningsOnSelectedDate"
         :key="i + screening">
-          <div class="col s3">
-            {{ screening.time }}
+          <div class="col s4 valign-wrapper">
+            <h5>{{ screening.time }}</h5>
           </div>
-          <div class="col s9">
-            <span class="col s12">{{ screening.auditorium }}</span>
-            <span class="col s12">Platser kvar</span>
+          <div class="col s8 valign-wrapper">
+            <p class="left-align">{{ screening.auditorium }}</p>
+            <p class="">Platser kvar</p>
           </div>
-        </div>
-      </div>
+        </li>
+      </ul>
+        
     </div>
+
   </div>
 </template>
 
@@ -98,10 +98,19 @@ export default {
 </script>
   
 <style scoped>
+  .date-btn-row{
+    margin-bottom: 1px;
+  }
+  .dates-row{
+    margin-bottom: 1px;
+  }
   .no-margin{
     margin-bottom: 0px;
   }
   .btn:hover{
     background-color: #1e88e5 !important;
+  }
+  .hidden{
+    border: 0px;
   }
 </style>

5acbe6df212ae6359faca4f512ecadf2e8a1f96f
snigelkott <dahlstedt.per@gmail.com>
2020-01-28 10:07:07 +0100

    cleaned up store

--- filmvisarna_ab/src/store/index.js
+++ filmvisarna_ab/src/store/index.js
@@ -6,32 +6,25 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    publishMovies: false,
     movies: [],
     screenings: [] ,           
   },
   mutations: {
-    movieShowing(state, value) {
-      state.movieDisplay = value;
-    },
     setMovies(state, data){
       state.movies = data
     },
     setScreenings(state, data){
       state.screenings = data;
     },
-    publishMovies(state){
-      state.publishMovies=true;
-    }
   },
   actions: {
     async getMovies({commit}){
-      let querySnapshot = await db.collection("movies").get()
-      let data = []
+      let querySnapshot = await db.collection("movies").get();
+      let data = [];
       querySnapshot.forEach((document) => {
-        data.push(document.data())
+        data.push(document.data());
       })
-      commit('setMovies', data)
+      commit('setMovies', data);
      },
      async getScreenings({commit}){
       let querySnapshot = await db.collection("screenings").get();
@@ -43,14 +36,6 @@ export default new Vuex.Store({
       })
       commit('setScreenings', screenings);
      },
-     async publishMovies({commit}){
-      let documents = require('@/data/movies.json')
-      for(let document of documents){
-       let res = await db.collection('movies').add(document)
-       console.log('publishmovies res' + res)
-      } 
-      commit('publishMovies') 
-    }
   },
   modules: {
   }

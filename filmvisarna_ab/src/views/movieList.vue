d717b755a867e8e424aa6011429caa15b5aa4d33
Andreas Lojdahl <andreas.lojdahl@gmail.com>
2020-01-28 10:44:46 +0100

    cleaned movielist

--- filmvisarna_ab/src/views/movieList.vue
+++ filmvisarna_ab/src/views/movieList.vue
@@ -1,25 +1,30 @@
 <template>
 
-  <ul class="container col">
-      <li v-for="(movie, i) in movies"
-        v-bind:key="movie.title + i"
-        class="container movie-items" 
-        @click="toMovieShowing(movie)">
+<ul class="container col">
+    <li v-for="(movie, i) in movies"
+    v-bind:key="movie.title + i"
+    class="container movie-items" 
+    @click="toMovieShowing(movie)">
+
         <div class="container posterDiv">
             <img :src="movie.images[0]" alt="MoviePoster" class="responsive-img" id="moviePoster">
         </div>
+
         <div class="movieInfo" :style="{ backgroundImage: 'url(' + movie.images[1]  + ')', backgroundPosition:'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover',}">
             <div class="colorBackground container col">
-            <h2 class="flow-text">{{ movie.title }}</h2>
-            <p class="flow-text">{{ movie.genre }}</p>
+                <p class="flow-text movietitle">{{ movie.title }}</p>
+                <p class="flow-text moviegenre">{{ movie.genre }}</p>
             </div>
         </div>
-      </li>
+
+    </li>
   </ul>
+
 </template>
 
 <script>
 export default {
+
     computed: {
         movies() {
             return this.$store.state.movies;
@@ -27,12 +32,11 @@ export default {
     },
     methods:{
         toMovieShowing(movie){
-            this.$store.commit('movieShowing', movie);
             this.$router.push({path:'/movies/'+ movie.movieId})
         }
     },
-        created(){
-      this.$store.dispatch("getMovies")
+    created(){
+        this.$store.dispatch("getMovies")
     },
 }
 </script>
@@ -40,33 +44,38 @@ export default {
 <style scoped>
 img{
     max-width: 20vw;
-    height: auto;
-    
+    height: auto;    
 }
+
 .colorBackground{
-    
     justify-content: flex-end;
     padding-left: 3%;
     padding-bottom: 3%;
-         background-image: linear-gradient(bottom, rgba(0, 0, 0, 0.9), rgb(0, 0, 0, 0.2)); 
+    background-image: linear-gradient(bottom, rgba(0, 0, 0, 0.9), rgb(0, 0, 0, 0.2)); 
     height: 100%;
     width: 100%;
 }
+
 .movie-items{
     justify-items: center;
     margin-top: 1.5%;
 }
+
 #moviePoster{
-    width: 100%;
+    width: 95%;
 }
+
 ul{
-    align-items: center
-    
+    align-items: center   
 }
 .posterDiv{
     width: 30%
 }
 
+.movietitle{
+    
+    margin-bottom: 0%;
+}
 .movieInfo{
     width: 100%
 }

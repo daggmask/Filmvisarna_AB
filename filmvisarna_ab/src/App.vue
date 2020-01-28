ea8c3ce52e2bfdc14551d8d3b7674891634677c2
snigelkott <dahlstedt.per@gmail.com>
2020-01-28 10:07:40 +0100

    cleaned up app

--- filmvisarna_ab/src/App.vue
+++ filmvisarna_ab/src/App.vue
@@ -2,14 +2,12 @@
 
 <template>
   <div id="app">
-    <!--<a @click="publishMovies" class="btn-floating pulse"><i class="material-icons">publish</i></a>-->
-    <mainHeader></mainHeader>
+    <mainHeader />
     <main>
-    <Menu id="navbar"></Menu>
-    <router-view></router-view>
+      <Menu id="navbar" />
+      <router-view />
     </main>
-    <Footer></Footer>
-    <!-- Menu -->
+    <Footer />
   </div>
 </template>
 
@@ -17,18 +15,13 @@
 import mainHeader from '@/components/header.vue'
 import Footer from '@/components/footer.vue'
 import Menu from '@/components/menu.vue'
+
 export default {
   components:{
     Footer,
     Menu,
     mainHeader
   },
-  
-    methods:{
-      publishMovies(){
-        this.$store.dispatch("publishMovies")
-      }
-    },
 }
 </script>
 
@@ -40,22 +33,12 @@ export default {
   color: white;
   font-family: Arial, Helvetica, sans-serif;
 }
-.container{
-  display: flex;
-}
-.col{
-  flex-direction: column;
-}
 #app{
   width: 100vw;
   min-height: 100vh;
   background-color: rgb(0, 0, 0);
   flex-direction: column;
   display: flex;
-  
-}
-main{
-  flex: 1;
 }
 #navbar{
   z-index: 999;

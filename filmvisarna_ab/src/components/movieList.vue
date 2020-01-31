67e77b646c0ac80c7ed98551665476685f5b22de
snigelkott <dahlstedt.per@gmail.com>
2020-01-31 10:24:14 +0100

    added responsive styling

--- filmvisarna_ab/src/components/movieList.vue
+++ filmvisarna_ab/src/components/movieList.vue
@@ -2,17 +2,17 @@
 <div class="container">
    <ul class="row">
     <li v-for="(movie, i) in movies"
-      v-bind:key="movie.title + i"
-      class="col s12 l4" 
-      @click="toMovieShowing(movie)">
-        <div class="card horizontal black">
-          <div class="card-image">
-           <img :src="movie.images[0]" :alt="movie.title + ' poster'">
-          </div>
-          <div class="card-stacked">
-          <div class="card-content text-flow">
-           <p>{{movie.title}}</p>
-           <p class="text-flow">{{movie.genre}}</p>
+    v-bind:key="movie.title + i"
+    class="col s12 m6 l4" 
+    @click="toMovieShowing(movie)">
+      <div class="card horizontal black">
+        <div class="card-image">
+          <img :src="movie.images[0]" :alt="movie.title + ' poster'">
+        </div>
+        <div class="card-stacked" :style="{backgroundImage: 'url(' +  movie.images[1] + ')', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}">
+          <div class="card-content">
+            <h6>{{movie.title}}</h6>
+            <p>{{movie.genre}}</p>
           </div>
         </div>
       </div>
@@ -44,6 +44,31 @@ export default {
 
 
 <style scoped>
+li{
+  height: 15vh;
+  margin-bottom: 1%;
+}
+.card{
+  height: 15vh;
+}
+.card-image{
+  width: auto;
+}
+img{
+  height: 100%;
+  width: auto;
+}
+.card-content{
+  padding: 4% !important;
+}
 
+@media screen and (min-width: 993px) {
+  li{
+    height: 30vh;
+  }
+  .card{
+    height: 30vh;
+  }
+}
 
 </style>

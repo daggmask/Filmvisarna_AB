349b57faa83a75da3b4bab6c9d4174a049cebda2
Martin Hellström <martver12@gmail.com>
2020-02-03 09:27:42 +0100

    Removed unused class styling

--- filmvisarna_ab/src/components/footer.vue
+++ filmvisarna_ab/src/components/footer.vue
@@ -3,13 +3,13 @@
         <div class="container col"> 
             <div class="row">
                 <a class="col s4" href="mailto:filmvisarnaab@gmail.com" target="_top">
-                    <p class="center-align valign-wrapper"><i class="material-icons iconfooter">email</i><span class="hide-on-med-and-down">filmvisarnaab@gmail.com</span></p>
+                    <p class="valign-wrapper"><i class="material-icons iconfooter">email</i><span class="hide-on-med-and-down">filmvisarnaab@gmail.com</span></p>
                 </a>
                 <a class="col s4" href="https://www.google.se/maps/place/Kalkstensv%C3%A4gen+3B,+224+78+Lund/@55.6917221,13.2098434,17z/data=!3m1!4b1!4m5!3m4!1s0x465397960c2a01df:0xb1c484736240c8d8!8m2!3d55.6917221!4d13.2120321" target="_blank">
-                    <p class="center-align valign-wrapper"><i class="material-icons iconfooter">home</i><span class="hide-on-med-and-down">Kalkstensvägen 3B, Lund</span></p>
+                    <p class="valign-wrapper"><i class="material-icons iconfooter">home</i><span class="hide-on-med-and-down">Kalkstensvägen 3B, Lund</span></p>
                 </a>
                 <a class="col s4" href="tel:+46700052668">
-                    <p class="center-align valign-wrapper"><i class="material-icons iconfooter">call</i><span class="hide-on-med-and-down">+46700052668</span></p>
+                    <p class="valign-wrapper"><i class="material-icons iconfooter">call</i><span class="hide-on-med-and-down">+46700052668</span></p>
                 </a>
             </div>
             <div class="row">

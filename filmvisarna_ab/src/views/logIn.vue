0f8ca11d7c6443ef92093c39878cf615f9b1a578
Fredrik Jönsson <Fredrikjay@gmail.com>
2020-02-04 10:59:46 +0100

    added new login form and change the css to materalize

--- filmvisarna_ab/src/views/logIn.vue
+++ filmvisarna_ab/src/views/logIn.vue
@@ -1,51 +1,38 @@
 <template>
-  <div>
-    <div class="imgcontainer">
-      <img src="@/images/img_avatar2.png" alt="Avatar" class="avatar" />
-    </div>
+<div class="loginPage row container ">
+<div class="card medium ">
+<form class="login-form">
 
-    <div class="container">
-      <label for="uname">
-        <b>Username</b>
-      </label>
-      <input type="text" placeholder="Enter Username" name="uname" required />
-
-      <label for="psw">
-        <b>Password</b>
-      </label>
-      <input type="password" placeholder="Enter Password" name="psw" required />
-
-      <button type="submit">Login</button>
-      <label>
-        <br />
-        <input type="checkbox" checked="checked" name="remember" /> Remember me
-      </label>
-    </div>
-
-    <div class="container" style="background-color:#f1f1f1">
-      <button type="button" class="cancelbtn">Cancel</button>
-      <br />
-      <span class="psw">
-        
-        <a href="#"> Forgot password?</a>
-      </span>
-    </div>
+  <div class="login-input">
+  <label for="username">användar namn</label>
+  <input type="text">
   </div>
+
+  <br>
+
+   <div class="login-input">
+  <label for="Password">Lösenord</label>
+  <input type="text">
+
+  </div>
+
+  <br>
+
+  <button class="btn waves-effect waves-light">login</button>
+
+
+
+</form>
+</div>
+</div>
 </template>
 
 <script>
-var modal = document.getElementById("id01");
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
 </script>
 
 <style>
-*{
+/* *{
   align-content: center;
 }
 form {
@@ -54,7 +41,7 @@ form {
 }
 
 /* Full-width inputs */
-input[type="text"],
+/*input[type="text"],
 input[type="password"] {
   width: 30%;
   padding: 12px 20px;
@@ -65,7 +52,7 @@ input[type="password"] {
 }
 
 /* Set a style for all buttons */
-button {
+/*button {
   background-color: #4caf50;
   
   padding: 20px 20px;
@@ -76,30 +63,30 @@ button {
 }
 
 /* Add a hover effect for buttons */
-button:hover {
+/*button:hover {
   opacity: 0.5;
 }
 
 /* Extra style for the cancel button (red) */
-.cancelbtn {
+/*.cancelbtn {
   width: 30%;
   padding: 10px 18px;
   background-color: #f44336;
 }
 
-/* Center the avatar image inside this container */
+/* Center the avatar image inside this container 
 .imgcontainer {
   text-align: center;
   margin: 24px 0 12px 0;
 }
 
-/* Avatar image */
+/* Avatar image 
 img.avatar {
   width: 10%;
   border-radius: 15%;
 }
 
-/* Add padding to containers */
+/* Add padding to containers 
 .container {
   padding: 16px;
   flex-direction: column;
@@ -107,13 +94,13 @@ img.avatar {
   
 }
 
-/* The "Forgot password" text */
+/* The "Forgot password" text 
 span.psw {
   float: right;
   padding-top: 16px;
 }
 
-/* Change styles for span and cancel button on extra small screens */
+/* Change styles for span and cancel button on extra small screens 
 @media screen and (max-width: 300px) {
   span.psw {
     display: block;
@@ -122,5 +109,5 @@ span.psw {
   .cancelbtn {
     width: 30%;
   }
-}
+} */
 </style>

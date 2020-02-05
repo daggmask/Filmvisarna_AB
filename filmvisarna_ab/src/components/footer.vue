dcc65377fe82043c82d2fedfc74dde76f0f17389
snigelkott <dahlstedt.per@gmail.com>
2020-02-05 13:27:49 +0100

    added text to footer

--- filmvisarna_ab/src/components/footer.vue
+++ filmvisarna_ab/src/components/footer.vue
@@ -1,22 +1,31 @@
 <template>
-    <footer class="page-footer light-blue darken-4 footback">
-        <div class="container col"> 
-            <div class="row">
-                <a class="col s4" href="mailto:filmvisarnaab@gmail.com" target="_top">
-                    <p class="center-block center-align"><i class="material-icons iconfooter">email</i><span class="hide-on-med-and-down">filmvisarnaab@gmail.com</span></p>
-                </a>
-                <a class="col s4" href="https://www.google.se/maps/place/Kalkstensv%C3%A4gen+3B,+224+78+Lund/@55.6917221,13.2098434,17z/data=!3m1!4b1!4m5!3m4!1s0x465397960c2a01df:0xb1c484736240c8d8!8m2!3d55.6917221!4d13.2120321" target="_blank">
-                    <p class="center-block center-align"><i class="material-icons iconfooter">home</i><span class="hide-on-med-and-down">Kalkstensvägen 3B, Lund</span></p>
-                </a>
-                <a class="col s4" href="tel:+46700052668">
-                    <p class="center-block center-align"><i class="material-icons iconfooter">call</i><span class="hide-on-med-and-down">+46700052668</span></p>
-                </a>
-            </div>
-            <div class="row">
-             <p class="col s12 center-align valign-wrapper">© 2020 Copyright Filmvisarna AB</p>
-            </div>
+<footer class="page-footer light-blue darken-4">
+    <div class="container col"> 
+        <div class="row small-margin">
+            <a class="col s4 center-align" href="mailto:filmvisarnaab@gmail.com" target="_top">
+                <div class="valign-wrapper">
+                    <i class="material-icons iconfooter">email</i><span class="hide-on-med-and-down">filmvisarnaab@gmail.com</span>
+                </div>
+                <span class="hide-on-large-only">Maila oss</span>
+            </a>
+            <a class="col s4 center-align" href="https://www.google.se/maps/place/Kalkstensv%C3%A4gen+3B,+224+78+Lund/@55.6917221,13.2098434,17z/data=!3m1!4b1!4m5!3m4!1s0x465397960c2a01df:0xb1c484736240c8d8!8m2!3d55.6917221!4d13.2120321" target="_blank">
+                <div class="valign-wrapper">
+                    <i class="material-icons">home</i><span class="hide-on-med-and-down">Kalkstensvägen 3B, Lund</span>
+                </div>
+                <span class="hide-on-large-only">Hitta hit</span>
+            </a>
+            <a class="col s4 center-align" href="tel:+46700052668">
+                <div class="valign-wrapper">
+                    <i class="material-icons iconfooter">call</i><span class="hide-on-med-and-down">+46700052668</span>
+                </div>
+                <span class="hide-on-large-only">Ring oss</span>
+            </a>
         </div>
-    </footer>
+        <div class="row small-margin">
+            <p class="col s12 center-align valign-wrapper">© 2020 Copyright Filmvisarna AB</p>
+        </div>
+    </div>
+</footer>
 </template>
 
 <script>
@@ -26,8 +35,15 @@ export default {
 </script>
 
 <style scoped>
-.footback{
+footer{
     background-color: #01579b;
     width: 100%;
+    padding-top: 10px
+}
+.small-margin{
+    margin-bottom: 5px;
+}
+.valign-wrapper{
+    justify-content: center
 }
 </style>

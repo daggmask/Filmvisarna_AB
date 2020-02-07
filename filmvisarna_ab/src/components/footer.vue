46ca0c33adcf9e901c0dd4ae1dd5d2cff3fccb31
Hassan <pkhajeh@gmail.com>
2020-02-07 14:29:27 +0100

    I put facebook and instagram Icons on footer

--- filmvisarna_ab/src/components/footer.vue
+++ filmvisarna_ab/src/components/footer.vue
@@ -1,33 +1,71 @@
 <template>
-    <footer class="page-footer light-blue darken-4 footback">
-        <div class="container col"> 
-            <div class="row">
-                <a class="col s4" href="mailto:filmvisarnaab@gmail.com" target="_top">
-                    <p class="center-block center-align"><i class="material-icons iconfooter">email</i><span class="hide-on-med-and-down">filmvisarnaab@gmail.com</span></p>
-                </a>
-                <a class="col s4" href="https://www.google.se/maps/place/Kalkstensv%C3%A4gen+3B,+224+78+Lund/@55.6917221,13.2098434,17z/data=!3m1!4b1!4m5!3m4!1s0x465397960c2a01df:0xb1c484736240c8d8!8m2!3d55.6917221!4d13.2120321" target="_blank">
-                    <p class="center-block center-align"><i class="material-icons iconfooter">home</i><span class="hide-on-med-and-down">Kalkstensvägen 3B, Lund</span></p>
-                </a>
-                <a class="col s4" href="tel:+46700052668">
-                    <p class="center-block center-align"><i class="material-icons iconfooter">call</i><span class="hide-on-med-and-down">+46700052668</span></p>
-                </a>
-            </div>
-            <div class="row">
-             <p class="col s12 center-align valign-wrapper">© 2020 Copyright Filmvisarna AB</p>
-            </div>
+  <footer class="page-footer light-blue darken-4 footback">
+    <div class="container col">
+      <div class="row">
+        <div class="col s4">
+          <div class="col">
+            <a class="col" href="mailto:filmvisarnaab@gmail.com" target="_top">
+              <p class="center-block center-align">
+                <i class="material-icons iconfooter">email</i>
+                <span class="hide-on-med-and-down">filmvisarnaab@gmail.com</span>
+              </p>
+            </a>
+          </div>
+          <div class="col">
+            <a class="col s4" href="https://www.instagram.com/">
+              <p class="center">
+                <img
+                  width="30px"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/1024px-Instagram_logo_2016.svg.png"
+                />
+                <span class="hide-on-med-and-down"></span>
+              </p>
+            </a>
+<br>
+<br>
+<br>
+            <a class="col s4" href="https://www.facebook.com/">
+              <p class="left-align">
+                <img
+                  width="30px"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Facebook-icon-1.png/600px-Facebook-icon-1.png"
+                />
+                <span class="hide-on-med-and-down"></span>
+              </p>
+            </a>
+          </div>
         </div>
-    </footer>
+        <a
+          class="col s4"
+          href="https://www.google.se/maps/place/Kalkstensv%C3%A4gen+3B,+224+78+Lund/@55.6917221,13.2098434,17z/data=!3m1!4b1!4m5!3m4!1s0x465397960c2a01df:0xb1c484736240c8d8!8m2!3d55.6917221!4d13.2120321"
+          target="_blank"
+        >
+          <p class="center-block center-align">
+            <i class="material-icons iconfooter">home</i>
+            <span class="hide-on-med-and-down">Kalkstensvägen 3B, Lund</span>
+          </p>
+        </a>
+        <a class="col s4" href="tel:+46700052668">
+          <p class="center-block center-align">
+            <i class="material-icons iconfooter">call</i>
+            <span class="hide-on-med-and-down">+46700052668</span>
+          </p>
+        </a>
+      </div>
+      <div class="row">
+        <p class="col s12 center-align valign-wrapper">© 2020 Copyright Filmvisarna AB</p>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style scoped>
-.footback{
-    background-color: #01579b;
-    width: 100%;
+.footback {
+  background-color: #01579b;
+  width: 100%;
 }
 </style>

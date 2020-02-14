58b6f5c4303d06cce4a631f6d704f7bd02d981e1
Martin Hellström <martver12@gmail.com>
2020-02-14 11:28:59 +0100

    resized text in showing

--- filmvisarna_ab/src/views/movieShowing.vue
+++ filmvisarna_ab/src/views/movieShowing.vue
@@ -12,9 +12,9 @@
                 <div class="card-stacked">
                     <div class="card-content valign-wrapper">
                         <div class="hide-on-med-and-down">
-                            <p class="medium-text">{{movie.genre}}</p>
-                            <p class="small-text">{{movie.length}} min</p>
-                            <p class="small-text">{{movie.language}}</p>
+                            <p class="large-text">{{movie.genre}}</p>
+                            <p class="large-text">{{movie.length}} min</p>
+                            <p class="large-text">{{movie.language}}</p>
                             <p class="small-margin large-text">{{ movie.description }}</p>
                         </div>
                         <div class="hide-on-large-only">

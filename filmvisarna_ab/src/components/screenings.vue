97aa441e44661754434c96c34137f33b1f7cd3df
snigelkott <dahlstedt.per@gmail.com>
2020-02-17 10:12:23 +0100

    changed to materialize dropdown

--- filmvisarna_ab/src/components/screenings.vue
+++ filmvisarna_ab/src/components/screenings.vue
@@ -1,22 +1,25 @@
 <template>
-<div class="container col screenings">
-
+<div class="col">
     <div class="row small-margin">
-        <a href="javascript:void(0)" class="btn-large light-blue darken-4 col s12 m6 offset-m3 xl4 offset-xl4" @click="isOpen = !isOpen">
-            <h5>{{ date }}</h5>
+        <a href="javascript:void(0)"
+        class="dropdown-trigger btn light-blue darken-4 col s12 m6 offset-m3 l4 offset-l4"
+        data-target="dropdown-menu">
+            {{ date }}
         </a>
+        <ul class="dropdown-content" id="dropdown-menu">
+            <li
+            class="light-blue darken-2 dropDownContent valign-wrapper"
+            :class="{firstDropdownContent: i === 0}"
+            v-for="(date, i) in dates" :key="i+date"
+            @click="setDate(date), screeningsAreShowing = true">
+                {{ date }}
+            </li>
+        </ul>
     </div>
 
-    <div class="dates-div" v-if="isOpen">
-        <div class="row small-margin" v-for="(date, i) in dates" :key="i + date">
-            <a href="javascript:void(0)" class="btn-large light-blue darken-2 col s12 m6 offset-m3 xl4 offset-xl4" @click="setDate(date), isOpen = !isOpen, screeningsAreShowing = true">
-                <h5>{{ date }}</h5>
-            </a>
-        </div>
-    </div>
-
-    <div class="row" v-if="!isOpen">
-      <ul :class="{ hidden: !screeningsAreShowing }" class="collection col s12 m6 offset-m3 xl4 offset-xl4">
+    <div class="row">
+      <ul class="collection col s12 m6 offset-m3 xl4 offset-xl4"
+      :class="{ hidden: !screeningsAreShowing }">
         <li class="collection-item col s12 light-blue darken-2"
         v-for="(screening, i) in screeningsOnSelectedDate"
         :key="i + screening">
@@ -88,7 +91,7 @@ export default {
             return screeningTime;
         },
         getDateAsString(date) {
-            return `${date.getDate()} ${this.getMonthName(date.getMonth())} ${date.getFullYear()}`
+            return `${date.getDate()} ${this.getMonthName(date.getMonth()).toUpperCase()} ${date.getFullYear()}`
         },
         getMonthName(monthNumber) {
             let months = ["januari", "februari", "mars", "april", "maj", "juni", "juli", "augusti", "september", "oktober", "november", "december"]
@@ -107,44 +110,46 @@ export default {
             return hourNumber;
         }
     },
-    created() {
-        this.$store.dispatch("getScreenings");
-    },
     data() {
         return {
             date: 'Välj datum',
             isOpen: false,
             screeningsAreShowing: false,
         }
+    },
+    mounted() {
+        let elems = document.querySelectorAll(".dropdown-trigger")
+        this.$M.Dropdown.init(elems);
     }
 }
 </script>
 
 <style scoped>
+.btn{
+    font-size: 1.5rem;
+}
+.firstDropdownContent{
+    border-top: 1px solid white;
+}
+.dropDownContent{
+    border-bottom: 1px solid white;
+    border-left: 1px solid white;
+    border-right: 1px solid white;
+    justify-content: center;
+    font-size: 1.5rem;
+}
 .small-margin {
     margin-bottom: 5px;
 }
-
-.no-margin {
-    margin-bottom: 0px;
-}
-
-.btn-large:hover {
-    background-color: #0091ea !important;
-}
-
 .hidden {
     border: 0px;
 }
-
 .screening-info {
     margin: 0;
 }
-
 .screening-time {
     text-align: center;
 }
-
 .collection {
     padding: 0;
 }

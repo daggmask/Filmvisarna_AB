150ea7f98a41a17e1631b736a47697ffa1bb3e17
Fredrik Jönsson <Fredrikjay@gmail.com>
2020-02-17 14:52:25 +0100

    added a home button and css to confirmation

--- filmvisarna_ab/src/components/bookingConfirmation.vue
+++ filmvisarna_ab/src/components/bookingConfirmation.vue
@@ -1,18 +1,21 @@
 <template>
-<div class="main container">
+<div class="main container col center">
+    <div class = "card blue darken-3">
     <div class="row">
-        <h6 class="col s12 title">Tack för din bokning!</h6>
+        <h5 class="col s12 title">Tack för din bokning!</h5>
         <p class="col s12">{{booking.screeningTitle}} - {{booking.screeningTime}}</p>
         <p class="col s12"> {{booking.screeningDate}} </p>
     </div>
     <div class="row">
-        <h6 class="col s12">Bokningsnummer : {{booking.customerBookingReferenceNumber}}</h6>
+        <p class="col s12">Bokningsnummer : {{booking.customerBookingReferenceNumber}}</p>
         <p class="col s12" v-if="booking.regularTickets != 0">Standardbiljetter : {{booking.regularTickets}}</p>
         <p class="col s12" v-if="booking.childTickets != 0">Barnbiljetter : {{booking.childTickets}}</p>
         <p class="col s12" v-if="booking.seniorCitizenTickets != 0">Pensionärbiljetter : {{booking.seniorCitizenTickets}}</p>
         <p class="col s12">Pris: {{booking.totalPriceForPurchase}}kr</p>
         <p class="col s12">Email: {{booking.account}}</p>
     </div>
+    </div>
+    <button type="submit" class="btn light-blue darken-4 waves-light">hem</button>
 </div>
 </template>
 
@@ -32,8 +35,11 @@ export default {
 </script>
 
 <style scoped>
+.card {
+  border-radius: 15%;
+
+}
 .main {
-    flex-direction: column;
     padding-top: 15%;
 
 }
@@ -42,7 +48,4 @@ p {
     font-size: 1.5em;
 }
 
-h6 {
-    font-size: 1.35rem;
-}
 </style>
